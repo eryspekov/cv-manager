5fe721d8a36f8556664cd188069b5f98749e4f28
boy_temper <!Qaz@Wsx147150>
2021-05-12 18:44:47 +0600

    init project

---
+++ src/main/kotlin/kg/coding/CvManagerApplication.kt
@@ -0,0 +1,11 @@
+package kg.coding
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CvManagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<CvManagerApplication>(*args)
+}
